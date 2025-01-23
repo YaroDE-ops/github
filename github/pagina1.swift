dfb645810cc3de64887f85a769bf54136728bdbd
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-01-23 12:10:34 +0100

    layout aanpassen

---
+++ github/pagina1.swift
@@ -0,0 +1,18 @@
+//
+//  pagina1.swift
+//  github
+//
+//  Created by yaro demeulenaere on 23/01/2025.
+//
+
+import SwiftUI
+
+struct pagina1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    pagina1()
+}
