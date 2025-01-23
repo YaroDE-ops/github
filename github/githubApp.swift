42f102e92c0b875960a9eb16df877f5bbbc9f6a9
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-01-23 11:24:25 +0100

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by yaro demeulenaere on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
